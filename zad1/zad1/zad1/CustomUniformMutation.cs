93161af7707054bf0956027b54914aeeb4f37df9
pawci0 <pgalewicz@gmail.com>
2020-03-29 20:32:04 +0200

    added CustomUniformMutation with adaptive capabilities

---
+++ zad1/zad1/zad1/CustomUniformMutation.cs
@@ -0,0 +1,40 @@
+﻿using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Randomizations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zad1
+{
+    public class CustomUniformMutation : UniformMutation
+    {
+        int[] MutableGenesIndexes { get; set; }
+
+        private CustomUniformMutation(int[] mutableGenesIndexes) : base(mutableGenesIndexes) 
+        {
+            MutableGenesIndexes = mutableGenesIndexes;
+        }
+
+        public CustomUniformMutation GenerateAdaptedMutation()
+        {
+            for(int i=0; i < MutableGenesIndexes.Length; i++)
+            {
+                if(MutableGenesIndexes[i] == 1)
+                {
+                    MutableGenesIndexes[i] = 0;
+                    break;
+                }
+            }
+
+            return new CustomUniformMutation(MutableGenesIndexes);
+        }
+
+
+        public static CustomUniformMutation Create(int numberOfBits)
+        {
+            int[] mutableGenesIndexes = RandomizationProvider.Current.GetInts(numberOfBits, 0, 1);
+            return new CustomUniformMutation(mutableGenesIndexes);
+        }
+    }
+}

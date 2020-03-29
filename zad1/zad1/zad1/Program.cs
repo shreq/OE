99348757590a0a3de115697bc64a6944901f66a2
pawci0 <pgalewicz@gmail.com>
2020-03-29 20:42:52 +0200

    finished adaptive event handler

--- zad1/zad1/zad1/Program.cs
+++ zad1/zad1/zad1/Program.cs
@@ -54,12 +54,18 @@ namespace zad1
                 var bestFitness = geneticAlgorithm.BestChromosome.Fitness.Value;
 
                 var epsilon = Math.Abs(0.9 * bestFitness);
-                if (Math.Abs(bestFitness - previouslyBestFitness) < epsilon &&
-                    geneticAlgorithm.Crossover is UniformCrossover)
+                if (Math.Abs(bestFitness - previouslyBestFitness) < epsilon)
                 {
-                    var mixProbability = (geneticAlgorithm.Crossover as UniformCrossover).MixProbability;
-                    geneticAlgorithm.Crossover = ParameterParser.ParserCrossover(1, (float)(0.9 * mixProbability));
+                    if (geneticAlgorithm.Crossover is UniformCrossover)
+                    {
+                        var mixProbability = (geneticAlgorithm.Crossover as UniformCrossover).MixProbability;
+                        geneticAlgorithm.Crossover = ParameterParser.ParserCrossover(1, (float)(0.9 * mixProbability));
+                    }
 
+                    if (geneticAlgorithm.Mutation is CustomUniformMutation)
+                    {
+                        geneticAlgorithm.Mutation = (geneticAlgorithm.Mutation as CustomUniformMutation).GenerateAdaptedMutation();
+                    }
 
                 }
             };

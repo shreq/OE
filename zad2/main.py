66d5675f1290f0044e3047b7eea0d0cca26108f4
shreq <shrek.kresh@o2.pl>
2020-04-26 17:59:00 +0200

    changing pymoo to platypus

--- zad2/main.py
+++ zad2/main.py
@@ -1,12 +1,9 @@
-from pymoo.algorithms.nsga2 import NSGA2
-from pymoo.algorithms.so_brkga import BRKGA
-from pymoo.algorithms.so_cmaes import CMAES
+import pymoo
+from matplotlib import pyplot
+from platypus import ZDT1, ZDT2, ZDT3, ZDT4, ZDT5, ZDT6
+from platypus.algorithms import NSGAII, IBEA, EpsMOEA, SPEA2, GDE3, EpsNSGAII
 from pymoo.factory import get_problem
-from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
-from pyrecorder.recorders.file import File
-from pyrecorder.recorders.gif import GIF
-from pyrecorder.video import Video
 
 from argument_parser import ArgumentParser
 
@@ -26,53 +23,52 @@ def plot_setup(problem_function, result_function):
     return plot
 
 
-def generate_video(history):
-    video = Video(GIF('animation.gif', duration=250))
-    for entry in history:
-        plot = Scatter(
-            title='Gen %s' % entry.n_gen
-        )
-        plot.add(
-            entry.pop.get('F')
-        )
-        plot.add(
-            entry.problem.pareto_front(),
-            plot_type='line',
-            color='black',
-            alpha=0.6
-        )
-        plot.do()
-
-        video.record()
-    video.close()
-
-
 argument_parser = ArgumentParser()
 
-algorithm = {
-    'brkga': BRKGA(),
-    'cmaes': CMAES(),
-    'nsga2': NSGA2()
-}[argument_parser.get_algorithm()]
-
-problem = get_problem(argument_parser.get_problem())
-
-result = minimize(
-    problem,
-    algorithm,
-    ('n_gen', argument_parser.get_n_generations()),
-    seed=1,
-    verbose=argument_parser.get_quiet(),
-    save_history=True
-)
-
+# result = minimize(
+#     problem,
+#     algorithm,
+#     ('n_gen', argument_parser.get_n_generations()),
+#     seed=1,
+#     verbose=argument_parser.get_quiet(),
+#     save_history=True
+# )
+#
 pic = plot_setup(
-    problem.pareto_front(),
-    result.F
+    get_problem('zdt1').pareto_front(),
 ).save('pic')
 
-if argument_parser.get_animation():
-    generate_video(result.history)
+problem = {
+    'zdt1': ZDT1(),
+    'zdt2': ZDT2(),
+    'zdt3': ZDT3(),
+    'zdt4': ZDT4(),
+    'zdt5': ZDT5(),
+    'zdt6': ZDT6()
+}['zdt1']
 
-if argument_parser.get_plot():
-    pic.show()
+population_size = 50
+
+algorithm = {
+    'nsga2': NSGAII(problem, population_size),
+    'ibea': IBEA(problem, population_size),
+    # 'epsilon-moea': EpsMOEA(problem),
+    'spea2': SPEA2(problem, population_size),
+    'gde3': GDE3(problem, population_size),
+    # 'epsilon-nsga2': EpsNSGAII(problem)
+}['nsga2']
+
+algorithm.run(5000)
+
+figure = pyplot.figure()
+ax = figure.add_subplot()
+ax.scatter(
+    [s.objectives[0] for s in algorithm.result],
+    [s.objectives[1] for s in algorithm.result]
+)
+
+pyplot.savefig(
+    'pic',
+    dpi=200,
+    bbox_inches='tight'
+)
